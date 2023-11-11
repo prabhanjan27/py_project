2c4e5f0878a34203a68ddc618ca4df75c37777b2
Atharvtk <139335273+Atharvtk@users.noreply.github.com>
2023-11-11 16:13:04 +0530

    final

--- class2.py
+++ class2.py
@@ -1,27 +1,17 @@
 #It is responsible for checking whether a given expression with brackets is balanced, using a stack to match opening and closing brackets and ensuring their proper nesting.
+
 class BracketValidator:
-    def _init_(self):
-        self.bracket_map = {')': '(', '}': '{', ']': '['}
-<<<<<<< HEAD
+    def _init_(self, mapper):
+        self.mapper = mapper
 
     def is_balanced(self, expression):
-        stack = BracketStack()
-
-=======
-    def is_balanced(self, expression):
-        stack = BracketStack()
->>>>>>> d3ff1c99d1c7749d751a375cecda85f77c81a3aa
+        stack = Stack()
         for char in expression:
             if char in '([{':
                 stack.push(char)
             elif char in ')]}':
-                if not stack.is_empty() and stack.pop() == self.bracket_map[char]:
+                if not stack.is_empty() and stack.pop() == self.mapper.brackets[char]:
                     continue
                 else:
                     return False
-<<<<<<< HEAD
-
         return stack.is_empty()
-=======
-        return stack.is_empty()
->>>>>>> d3ff1c99d1c7749d751a375cecda85f77c81a3aa
